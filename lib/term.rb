41a706f4fc8ec65e253ec8cba08c2ccbb79c6196
longtime1116 <longtime1116@gmail.com>
2018-06-10 17:46:27 +0900

    add methods about checking overlap

--- lib/term.rb
+++ lib/term.rb
@@ -10,6 +10,84 @@ class Term
     post_validate!
   end
 
+  def overlap_with?(target)
+    if perfectly_indefinite? || target.perfectly_indefinite?
+      true
+    elsif from_indefinite?
+      if target.from_indefinite?
+        true
+      else
+        to >= target.from ? true : false
+      end
+    elsif to_indefinite?
+      if target.to_indefinite?
+        true
+      else
+        from <= target.to ? true : false
+      end
+    else
+      if target.from_indefinite?
+        from <= target.to ? true : false
+      elsif target.to_indefinite?
+        to >= target.from ? true : false
+      else
+        from <= target.to && to >= target.from ? true : false
+      end
+    end
+  end
+
+  def overlap_with(target)
+    return nil unless overlap_with?(target)
+
+    if perfectly_indefinite?
+      Term.new(from: target.from, to: target.to)
+    elsif target.perfectly_indefinite?
+      Term.new(from: from, to: to)
+    elsif from_indefinite?
+      if target.from_indefinite?
+        Term.new(to: [to, target.to].min)
+      elsif target.to_indefinite?
+        Term.new(from: target.from, to: to)
+      else
+        Term.new(from: target.from, to: [to, target.to].min)
+      end
+    elsif to_indefinite?
+      if target.from_indefinite?
+        Term.new(from: from, to: target.to)
+      elsif target.to_indefinite?
+        Term.new(from: [from, target.to].max)
+      else
+        Term.new(from: [from, target.from].max, to: target.to)
+      end
+    else
+      if target.from_indefinite?
+        Term.new(from: from, to: [to, target.to].min)
+      elsif target.to_indefinite?
+        Term.new(from: [from, target.from].max, to: to)
+      else
+        Term.new(from: [from, target.from].max, to: [to, target.to].min)
+      end
+    end
+  end
+
+  protected
+
+  def from_indefinite?
+    from.nil?
+  end
+
+  def to_indefinite?
+    to.nil?
+  end
+
+  def indefinite?
+    from_indefinite? || to_indefinite?
+  end
+
+  def perfectly_indefinite?
+    from_indefinite? && to_indefinite?
+  end
+
   private
 
   def pre_validate!(from, to)
@@ -36,9 +114,5 @@ class Term
 
   def proper_class?(date)
     [String, DateTime, Time, NilClass].any? {|klass| klass == date.class}
-  end
-
-  def indefinite?
-    from.nil? || to.nil?
   end
 end
